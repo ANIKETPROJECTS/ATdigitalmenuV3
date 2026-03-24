2e275658384014eadda7edfa9cb5ca70f56fe388
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-03-24 14:17:46 +0530

    Update application configuration for atdigitalmenu

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: "barrelborn",
-      script: "./dist/index.js",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3003,
-        MONGODB_URI: "mongodb+srv://abarrelborn_db_user:xTl95RKaa9k9MagM@barrelborn.fllyeem.mongodb.net/barrelborn?appName=BarrelBorn",
-        SESSION_SECRET: "your_random_session_secret_here"
-      }
+  apps: [{
+    name: "atdigitalmenu",
+    script: "./dist/index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3002,
+      MONGODB_URI: "mongodb+srv://sairajkoyande_db_user:a5RlALolj2OOnZ4q@atdigitalmenu.za2j2lz.mongodb.net/?appName=atdigitalmenu",
+      SESSION_SECRET: "f7k2$Xqp9mLzR4#vNwT8@cYdJeA3bQsU"
     }
-  ]
+  }]
 };
